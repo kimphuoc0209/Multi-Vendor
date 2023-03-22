9ebfa04161e6d782c7e3088db31d362854e8681b
Phuoc Tran <phuoc.tran@spiderbox.design>
2023-03-22 22:46:01 +0700

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,5 @@
+import LoginPage from "../src/pages/Login";
+import SignupPage from "../src/pages/SignupPage";
+import HomePage from "../src/pages/HomePage";
+
+export { LoginPage, SignupPage, HomePage };
