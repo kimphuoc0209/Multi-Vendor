cd475ea0359c8d556b1aace8922b2e7ccd4a1f50
Phuoc Tran <phuoc.tran@spiderbox.design>
2023-03-27 21:20:49 +0700

    UI User

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, SignupPage, HomePage } from "./Routes";
+import {
+  LoginPage,
+  SignupPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+  EventsPage,
+  FAQPage,
+  ProductDetailsPage,
+  CheckoutPage,
+  PaymentPage,
+  OrderSuccessPage,
+  ProfilePage,
+  ShopCreatePage,
+  ShopLoginPage
+} from "./Routes";
+import { ShopHomePage } from "./ShopRoutes";
 
 const App = () => {
   return (
@@ -10,6 +26,19 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product/:name" element={<ProductDetailsPage />} />
+        <Route path="/best-selling" element={<BestSellingPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/order/success/:id" element={<OrderSuccessPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        {/* shop Routes */}
+        <Route path="/shop-create" element={<ShopCreatePage />} />
+        <Route path="/shop-login" element={<ShopLoginPage />} />
+        <Route path="/shop/:id" element ={<ShopHomePage/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,31 @@
 import LoginPage from "../src/pages/Login";
 import SignupPage from "../src/pages/SignupPage";
 import HomePage from "../src/pages/HomePage";
+import ProductsPage from "../src/pages/ProductsPage";
+import BestSellingPage from "../src/pages/BestSellingPage";
+import EventsPage from "../src/pages/EventsPage";
+import FAQPage from "../src/pages/FAQPage";
+import ProductDetailsPage from "../src/pages/ProductDetailsPage";
+import CheckoutPage from "../src/pages/CheckoutPage";
+import PaymentPage from "../src/pages/PaymentPage";
+import OrderSuccessPage from "../src/pages/OrderSuccessPage";
+import ProfilePage from "../src/pages/ProfilePage";
+import ShopCreatePage from "../src/pages/ShopCreate";
+import ShopLoginPage from "../src/pages/ShopLoginPage.js";
 
-export { LoginPage, SignupPage, HomePage };
+export {
+  LoginPage,
+  SignupPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+  EventsPage,
+  FAQPage,
+  ProductDetailsPage,
+  CheckoutPage,
+  PaymentPage,
+  OrderSuccessPage,
+  ProfilePage,
+  ShopCreatePage,
+  ShopLoginPage
+};
